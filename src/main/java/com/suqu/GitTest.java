69353bfdad3f9d83fcb5416a3112b4cb01f48c22
Suqu <1064269197@qq.com>
2022-06-16 10:05:07 +0800

    first commit

---
+++ src/main/java/com/suqu/GitTest.java
@@ -0,0 +1,12 @@
+package com.suqu;
+
+/**
+ * @author: 10642
+ * @date: 2022/6/16 10:01
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
